0c755b4bbf9cbfd4a5b151d52775bb3c986d2e92
Rijk van Putten <rijk@rijkvp.nl>
2022-02-05 14:47:11 +0000

    Finish startup script + add PHP test

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 # Startup script to initialize the repository
+bold=$(tput bold)
+normal=$(tput sgr0)
 figlet Databases
-echo "\nDatabases met PHP, MySQL"
-echo "Door: Rijk van Putten\n"
+echo "\n${bold}Databases Module met PHP en MySQL"
+echo "${normal}Deze repository is voor je klaargezet door ${bold}Rijk van Putten${normal} 😃\n"
 
-echo "Starting Apache HTTP server.."               
-./scripts/start_apache.sh
+echo "Even geduld graag terwijl de volgende dingen worden opgezet:"
+echo "(1/3) Apache HTTP server.."               
+./scripts/start_apache.sh > /dev/null
 
-echo "Starting MySQL server.."               
+echo "(2/3) MySQL server.."               
 ./scripts/start_mysql.sh > /dev/null
 
 
-echo "Starting querier.."
+echo "(3/3) SQL Querier.."
 cd ./querier
 ./init_querier.sh > /dev/null
-node querier
+node querier > /dev/null &
+echo "Klaar! (hopelijk duurde het niet te lang)"
 
-echo "==================================="
-echo "Apache HTTP | http://localhost:8001"
-echo "SQL Querier | http://localhost:1700"
-echo "==================================="
+echo "\nJe Gitpod workspace is klaar voor gebruik!\nDruk op ${bold}http://localhost:8001${normal} om je websites te bekijken."
