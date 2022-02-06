ff67dbc7bcc08112274941fa50501b5a769fbd8b
Rijk van Putten <rijk@rijkvp.nl>
2022-02-06 13:23:58 +0000

    Add database import to startup

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -7,19 +7,22 @@ echo "\n${bold}Databases Module met PHP en MySQL"
 echo "${normal}Deze repository is voor je klaargezet door ${bold}Rijk van Putten${normal} 😃\n"
 
 echo "Even geduld graag terwijl de volgende dingen worden voorbereid:"
-echo "(1/4) Apache HTTP server starten.."               
+echo "(1/5) Apache HTTP server starten.."               
 ./scripts/start_apache.sh > /dev/null
 
-echo "(2/4) MySQL server starten.."               
+echo "(2/5) MySQL server starten.."               
 ./scripts/start_mysql.sh > /dev/null
 
-echo "(3/4) phpMyAdmin installeren.."               
+echo "(3/5) phpMyAdmin installeren.."               
 ./scripts/install_phpmyadmin.sh > /dev/null 2>&1
 
-echo "(4/4) SQL Querier installeren en opstarten.."
+echo "(4/5) SQL Querier installeren en opstarten.."
 ./scripts/init_querier.sh > /dev/null
 pkill -f "querier"
 node querier > /dev/null &
+
+echo "(5/5) Databases importeren.."
+./scripts/import_dbs.sh > /dev/null
 
 echo "Klaar! (hopelijk duurde het niet te lang)"
 
