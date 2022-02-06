5e5a6eae3b95663fd2ac99eb524940bd54462113
Rijk van Putten <rijk@rijkvp.nl>
2022-02-06 14:38:06 +0000

    Add GUI for importing/exporting

--- public/index.php
+++ public/index.php
@@ -1,3 +1,33 @@
+<?php
+
+// Execute database management scripts
+
+function runScript($fileName)
+{
+    $cmd = "cd .. && sh ./scripts/" . $fileName;
+    shell_exec($cmd);
+}
+
+$importStarted = false;
+$exportStarted = false;
+$restoreStarted = false;
+
+if ($_POST['import'] == 1) {
+    runScript("import_dbs.sh");
+    $importStarted = true;
+}
+
+if ($_POST['export'] == 1) {
+    runScript("export_dbs.sh");
+    $exportStarted = true;
+}
+
+if ($_POST['restore_default'] == 1) {
+    runScript("restore_default_dbs.sh");
+    $restoreStarted = true;
+}
+
+?>
 <!doctype html>
 <html lang="nl">
 
@@ -10,29 +40,21 @@
 </head>
 
 <body class="container">
-    <main class="m-5 p-5">
+    <main class="mx-5 p-5">
         <div class="text-center">
             <h1 class="fw-bold">Databases Module</h1>
             <p class="lead">Maak een dynamische website met een database.</p>
         </div>
-        <div class="row m-5">
+        <div class="row mx-5 p-5">
             <div class="col">
-                <h2>PHP Informatie</h2>
-                <?php
-                
-                echo "<p>PHP Versie: " . phpversion() . "</p>";
-
-                ?>
                 <h3>phpMyAdmin</h3>
                 <p>Login met <strong>root</strong> als gebruikersnaam zonder wachtwoord.</p>
                 <a class="btn btn-outline-primary" href="/phpmyadmin" title="phpMyAdmin" target="_blank">Open phpMyAdmin</a>
             </div>
             <div class="col">
-                <h2>MySQL Informatie</h2>
+                <h2>Database Beheer</h2>
                 <?php
-
                 $mysqli = new mysqli("localhost", "root");
-                echo "<p>MySQL Versie: " . $mysqli->server_version . "</p>";
                 if ($result = $mysqli->query("SHOW databases")) {
                     echo "<b>" . $result->num_rows . " databases: </b>";
                     echo "<ul>";
@@ -44,9 +66,40 @@
                 }
 
                 ?>
+                <?php
+                function info($msg)
+                {
+                    echo '<div class="alert alert-info" role="alert">' . $msg . '</div>';
+                }
+
+                if ($importStarted) {
+                    info("Databases geimporteerd");
+                }
+                if ($exportStarted) {
+                    info("Databases geexporteerd");
+                }
+                if ($restoreStarted) {
+                    info("Standaard databases hersteld");
+                }
+                ?>
+                <form action="index.php" method="post">
+                    <div class="vstack gap-2 col-md-5">
+                        <button class="btn btn-primary" name="export" value="1">Exporteer databases</button>
+                        <button class="btn btn-outline-primary" name="import" value="1">Importeer databases</button>
+                        <button class="btn btn-outline-danger" name="restore_default" value="1">Herstel standaard</button>
+                    </div>
+                </form>
+
             </div>
+
         </div>
     </main>
+    <footer>
+        <?php
+        echo "<p>PHP versie: " . phpversion() . "</p>";
+        echo "<p>MySQL versie: " . $mysqli->server_version . "</p>";
+        ?>
+    </footer>
 </body>
 
 </html>
