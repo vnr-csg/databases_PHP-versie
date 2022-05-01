833532ee936fabf75ff645624fc6e0f9f7ef31e5
Rijk van Putten <rijk@rijkvp.nl>
2022-05-01 18:21:25 +0000

    Try mysql startup fix

--- scripts/start_mysql.sh
+++ scripts/start_mysql.sh
@@ -14,5 +14,8 @@ then
     rm /var/run/mysqld/gitpod-init.lock
 fi
 
+gp await-port 3306
+gp await-port 33060
+
 # Create read user with read only privileges
 mysql -u root -e "CREATE USER IF NOT EXISTS 'read'@'localhost'; GRANT SELECT ON *.* TO 'read'@'localhost'; FLUSH PRIVILEGES;"
