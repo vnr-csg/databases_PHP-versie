a5e754552228f6f62da4b77c61b327d756592763
Rijk van Putten <rijk@rijkvp.nl>
2022-05-01 18:58:45 +0000

    Improve startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,20 +2,19 @@
 # Startup script to initialize the repository
 bold=$(tput bold)
 normal=$(tput sgr0)
-figlet Databases
-echo "\n${bold}Databases Module met PHP en MySQL"
-echo "${normal}Deze repository is voor je klaargezet door ${bold}Rijk van Putten${normal} 😃\n"
+figlet Databases Omgeving
+echo "\n${bold}Databases Omgeving"
+echo "${normal}Deze Gitpod-workspace is voor je klaargezet door ${bold}Rijk van Putten${normal} 😃\n"
 
-echo "Even geduld graag terwijl de volgende dingen worden voorbereid:"
-
-echo "Servers aan het opstarten.."
 ./scripts/start_apache.sh > /dev/null
+
 ./scripts/start_mysql.sh > /dev/null
 
-echo "Databases aan het importeren.."
+echo "Databases worden geïmporteerd (dit kan even duren).."
+start=`date +%s`
 ./scripts/import_dbs.sh > /dev/null
-
-
-echo "Klaar! (hopelijk duurde het niet te lang)"
+end=`date +%s`
+runtime=$( echo "$end - $start" | bc -l )
+echo "Klaar! Geïmporteerd in $runtime seconden. (hopelijk duurde het niet te lang)"
 
 echo "\nJe Gitpod workspace is klaar voor gebruik!\nDruk op ${bold}http://localhost:8001${normal} om naar het lesmateriaal te gaan en je websites te bekijken."
