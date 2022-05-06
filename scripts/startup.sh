8717a72c32a345c1f5fa24c34b30e35beeb9272f
Rijk van Putten <rijk@rijkvp.nl>
2022-05-06 13:07:29 +0000

    Add lockfile to prevent reimporting databases

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -7,14 +7,21 @@ echo "\n${bold}Databases Omgeving"
 echo "${normal}Deze Gitpod-workspace is voor je klaargezet door ${bold}Rijk van Putten${normal} 😃\n"
 
 ./scripts/start_apache.sh > /dev/null
-
 ./scripts/start_mysql.sh > /dev/null
 
-echo "Databases worden geïmporteerd (dit kan even duren).."
-start=`date +%s`
-./scripts/import_dbs.sh > /dev/null
-end=`date +%s`
-runtime=$( echo "$end - $start" | bc -l )
-echo "Klaar! Geïmporteerd in $runtime seconden. (hopelijk duurde het niet te lang)"
+LOCKFILE=".imported.lock"
+if test -f "$LOCKFILE"; then
+    echo "Databases zijn al geïmporteerd!"
+else
+    echo "Databases worden geïmporteerd.. Dit kan even duren!"
+    echo "Dit is alleen nodig als je een nieuwe workspace aanmaakt. Start deze workspace opnieuw op via de Gitpod interface op dit te vermijden."
+    start=`date +%s`
+    ./scripts/import_dbs.sh > /dev/null
+    end=`date +%s`
+    runtime=$( echo "$end - $start" | bc -l )
+    echo "Klaar! Geïmporteerd in $runtime seconden. (hopelijk duurde het niet te lang)"
+    touch $LOCKFILE
+fi
+
 
 echo "\nJe Gitpod workspace is klaar voor gebruik!\nDruk op ${bold}http://localhost:8001${normal} om naar het lesmateriaal te gaan en je websites te bekijken."
