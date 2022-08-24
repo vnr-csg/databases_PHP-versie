502612439d4b6a6b350593594f2f573d7bffca21
Rijk van Putten <rijk@rijkvp.nl>
2022-08-24 14:20:48 +0000

    Fix await-port deprecation

--- scripts/start_mysql.sh
+++ scripts/start_mysql.sh
@@ -14,8 +14,8 @@ then
     rm /var/run/mysqld/gitpod-init.lock
 fi
 
-gp await-port 3306
-gp await-port 33060
+gp ports await 3306
+gp ports await 33060
 
 # Create read user with read only privileges
 mysql -u root -e "CREATE USER IF NOT EXISTS 'read'@'localhost'; GRANT SELECT ON *.* TO 'read'@'localhost'; FLUSH PRIVILEGES;"
