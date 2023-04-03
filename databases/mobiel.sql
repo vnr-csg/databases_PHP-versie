94f218e07614a3c814592ab04f95a9b438b01e63
vnr-csg <vnr@csg.nl>
2023-04-03 18:32:23 +0000

    v0.51

--- databases/mobiel.sql
+++ databases/mobiel.sql
@@ -3,7 +3,7 @@ CREATE DATABASE `mobiel`;
 USE `mobiel`;
 
 CREATE TABLE `vrienden` (
-    `id` int PRIMARY KEY,
+    `id` int AUTO_INCREMENT PRIMARY KEY,
     `naam` text NOT NULL,
     `plaats` text NOT NULL
 );
